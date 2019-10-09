2bbd2b06f55c3f484e272928b0acb20c919279cf
ochadwick@storans.school.nz <ochadwick@storans.school.nz>
2019-10-10 11:13:47 +1300

    basic to check if pereson is eligable for superanuation

--- super.py
+++ super.py
@@ -1,9 +1,12 @@
-SUPER_AGE = 65
 
+# for superanuation, its a constant bc its not changed in the program
+SUPER_AGE = 65
+#a loop testing the different values
 keep_going = ""
 
 while keep_going == "":
     age = input("What is your age?")
+    #must be greater than or equal for 65
     if age >= SUPER_AGE:
         print("You get super")
     else:
